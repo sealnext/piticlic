73008ead26261943ca9ec7a7c00344dc3e6d52c9
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2024-02-13 22:51:26 +0200

    Hero page

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,94 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Index.module.css';
+import Image from 'next/image';
+
+// using next/image to optimize images
+import topBarSvg from "../public/index/topbar.svg";
+import norHomePng from "../public/index/nor-acasa.png";
+import norMagazinPng from "../public/index/nor-magazin.png";
+import norResursePng from "../public/index/nor-gratuit.png";
+import norContactPng from "../public/index/nor-contact.png";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <meta name="title" content="Home - educational games and educational software for PCs, tablets, mobile" />
+                <title>Piticlic.ro - home</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="PitiClic offers educational games and educational software for PC, tablets, phones. Games and interactive applications for children of all ages." />
+                <meta name="keywords" content="educational games, educational software, educational games for PC, educational games for tablets, educational games for mobile, educational software for PC, educational software for tablets, educational software for mobile" />
+                <link href="http://www.piticlic.ro/en/" rel="canonical" />
+                <link href="http://www.piticlic.ro/ro/" rel="alternate" hreflang="ro-RO" />
+            </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main>
+                <div className={styles.heroBackground}>
+                    <div className={styles.header}>
+                        <Image
+                            src={topBarSvg}
+                            alt="Follow us on Twitter"
+                            objectFit="contain"
+                            objectPosition="top"
+                            style={{
+                                width: "100%",
+                                height: "fit-content"
+                            }}
+                        />
+                        <Image
+                            src={norHomePng}
+                            alt="Home"
+                            className={styles.scaleHoverEffect}
+                            style={{
+                                position: 'absolute',
+                                width: '14%',
+                                top: '27%',
+                                left: '5%',
+                                zIndex: 2
+                            }}
+                        />
+                        <Image
+                            src={norMagazinPng}
+                            className={styles.scaleHoverEffect}
+                            alt="Magazin"
+                            style={{
+                                position: 'absolute',
+                                width: '14%',
+                                top: '26%',
+                                left: '19.7%',
+                                zIndex: 2
+                            }}
+                        />
+                        <Image
+                            src={norResursePng}
+                            alt="Resurse"
+                            className={styles.scaleHoverEffect}
+                            style={{
+                                position: 'absolute',
+                                width: '22%',
+                                top: '39%',
+                                left: '36%',
+                                zIndex: 2
+                            }}
+                        />
+                        <Image
+                            src={norContactPng}
+                            className={styles.scaleHoverEffect}
+                            alt="Contact"
+                            style={{
+                                position: 'absolute',
+                                width: '14%',
+                                top: '73%',
+                                left: '62%',
+                                zIndex: 2
+                            }}
+                        />
+                    </div>
+                </div>
+            </main>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer>
+            </footer>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+    );
 }
