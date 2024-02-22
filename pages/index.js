be58faeb429ff62db3db69c3a58e332db4af43ce
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2024-02-22 19:08:26 +0200

    avion

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import styles from '../styles/Index.module.scss';
 import animation from '../styles/animations.module.scss';
 
-import Head from 'next/head'; // head tag management
-import Image from 'next/image'; // image optimization
-import Link from 'next/link'; // client-side navigation
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import cd1Png from '../public/index/CV1.png';
 import cd2Png from '../public/index/cd2.png';
 import balonPng from '../public/index/ballon_text.webp';
+import avionPng from '../public/index/avion.webp';
+import promoPng from '../public/index/promo.png';
+import containerPng from '../public/index/container.webp';
 
 import Header from '../components/Header';
+
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+import { horizontalLoop } from '../util/_horizontalLoop';
 
 export default function Home() {
     const [isMenuChecked, setIsMenuChecked] = useState(false);
@@ -19,6 +27,49 @@ export default function Home() {
     const handleCheckboxChange = () => {
         setIsMenuChecked(!isMenuChecked);
     };
+
+    const wrapperRef = useRef(null);
+    const airplaneRef = useRef(null);
+    const boxesRef = useRef([]);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const loop = horizontalLoop(boxesRef.current, {
+            repeat: -1,
+            speed: 1,
+            snap: 1
+        });
+
+        // gsap animation for the airplane make it move from left to right of the screen
+        gsap.to(airplaneRef.current, {
+            x: '200vw',
+            y: '30vh',
+            duration: 13,
+            ease: 'none',
+            repeat: -1
+        });
+
+        loop.play();
+
+        return () => {
+            loop.kill();
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, []);
+
+    const items = [
+        'Digital Manuals',
+        'Fun Mathematics',
+        'Educational Software',
+        'Financial Education',
+        'Interactive Alphabet',
+        'Digital Manuals',
+        'Fun Mathematics',
+        'Educational Software',
+        'Financial Education',
+        'Interactive Alphabet'
+    ];
 
     return (
         <div>
@@ -54,9 +105,9 @@ export default function Home() {
                     <div className={styles.heroContent}>
                         <h2>Eliminăm Plictiseala din Educație™</h2>
                         <h1>
-                            DESCOPERA NOILE noastre
+                            DESCOPERA JOCURILE
                             <br />
-                            JOCURI EDUCATIVE
+                            NOASTRE EDUCATIVE
                         </h1>
                         <div className={styles.cdContainer}>
                             <Image
@@ -79,9 +130,41 @@ export default function Home() {
                         />
                     </Link>
                 </div>
+                <div className={styles.textMarquee}>
+                    <div className={styles.wrapper} ref={wrapperRef}>
+                        {items.map((item, index) => (
+                            <div className={styles.box} key={index} ref={el => boxesRef.current[index] = el}>
+                                <div className={styles.test}>{item}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={styles.avionAxis}>
+                    <div ref={airplaneRef} className={styles.airplaneComponent}>
+                        <Image
+                            className={styles.promoImage}
+                            src={promoPng}
+                        />
+                        <Image
+                            className={styles.avionImage}
+                            src={avionPng}
+                        />
+                    </div>
+                    <div className={styles.emptySpace} />
+                </div>
+                <div className={styles.waterImage}>
+                    <div className={animation.floatInWaterEffect}>
+                        <Image
+                            src={containerPng}
+                        />
+                    </div>
+                </div>
+                <div className={styles.footerImage}>
+
+                </div>
             </main >
 
             <footer></footer>
         </div >
     );
-}
+};
